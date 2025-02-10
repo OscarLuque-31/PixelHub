231372afc0215ace2d289e06d0d454ae6bdd42ca
OscarLuque-31 <oscar11luquexd@gmail.com>
2025-02-10 16:11:35 +0100

    añado mejoras visuales y empiezo pantalla perfil

--- src/main/java/controllers/GameItemListaController.java
+++ src/main/java/controllers/GameItemListaController.java
@@ -12,52 +12,61 @@ import models.Game;
 
 public class GameItemListaController {
 
-    @FXML private ImageView gameImage;
-    @FXML private HBox gamePlatforms;
-    @FXML private Label gameRating;
-    @FXML private Label gameTitle;
-    @FXML private Region spacer;
-    
-    public void setGameData(Game juego) {
-        gameTitle.setText(juego.getName());
-        
-        gameRating.setText("⭐ " + juego.getRating());
-        
-        gamePlatforms.getChildren().add(crearBloquePlataformas(juego));
+	@FXML 
+	private ImageView gameImage;
+	
+	@FXML 
+	private HBox gamePlatforms;
+	
+	@FXML 
+	private Label gameRating;
+	
+	@FXML 
+	private Label gameTitle;
+	
+	@FXML 
+	private Region spacer;
 
-        try {
-            gameImage.setImage(new Image(juego.getBackgroundImage()));
-        } catch (Exception e) {
-        	gameImage.setImage(new Image(getClass().getResource("/images/error.png").toExternalForm()));
-        }
-        
-        gameImage.setFitWidth(100); // Ancho fijo
-        gameImage.setFitHeight(70); // Alto fijo
-        gameImage.setPreserveRatio(false); // Evita que la imagen se ajuste al tamaño cambiando su relación de aspecto
-        gameImage.setSmooth(true); // Suaviza la imagen
-        gameImage.setCache(true);  // Optimiza la carga
+	public void setGameData(Game juego) {
+		gameTitle.setText(juego.getName());
 
-        // Clipping para recortar los lados sobrantes
-        Rectangle clip = new Rectangle(gameImage.getFitWidth(), gameImage.getFitHeight());
-        clip.setArcWidth(10);  // Opcional: Bordes redondeados
-        clip.setArcHeight(10);
-        gameImage.setClip(clip);
-    }
-    
-    private HBox crearBloquePlataformas(Game juego) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Plataformas.fxml"));
-            HBox plataformas = loader.load();
+		gameRating.setText("⭐ " + juego.getRating());
 
-            // Obtener el controlador del FXML
-            PlataformasController controller = loader.getController();
-            controller.setPlataformasFoto(juego);
+		gamePlatforms.getChildren().add(crearBloquePlataformas(juego));
 
-            return plataformas;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new HBox(new Label("Error cargando juego"));
-        }
-    }
+		try {
+			gameImage.setImage(new Image(juego.getBackgroundImage()));
+		} catch (Exception e) {
+			gameImage.setImage(new Image(getClass().getResource("/images/error.png").toExternalForm()));
+		}
+
+		gameImage.setFitWidth(100); // Ancho fijo
+		gameImage.setFitHeight(70); // Alto fijo
+		gameImage.setPreserveRatio(false); // Evita que la imagen se ajuste al tamaño cambiando su relación de aspecto
+		gameImage.setSmooth(true); // Suaviza la imagen
+		gameImage.setCache(true);  // Optimiza la carga
+
+		// Clipping para recortar los lados sobrantes
+		Rectangle clip = new Rectangle(gameImage.getFitWidth(), gameImage.getFitHeight());
+		clip.setArcWidth(10);  // Opcional: Bordes redondeados
+		clip.setArcHeight(10);
+		gameImage.setClip(clip);
+	}
+
+	private HBox crearBloquePlataformas(Game juego) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Plataformas.fxml"));
+			HBox plataformas = loader.load();
+
+			// Obtener el controlador del FXML
+			PlataformasController controller = loader.getController();
+			controller.setPlataformasFoto(juego);
+
+			return plataformas;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new HBox(new Label("Error cargando juego"));
+		}
+	}
 
 }
